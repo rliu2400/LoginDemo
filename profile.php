39dfd27e24ac8845dfcd2cf58767e602a2f939b6
rliu2400 <rliu2400@gmail.com>
2022-07-24 14:32:39 -0500

    made login sql injection safe

--- profile.php
+++ profile.php
@@ -17,11 +17,11 @@ if (mysqli_connect_errno()) {
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 // We don't have the password or email info stored in sessions so instead we can get the results from the database.
-$stmt = $con->prepare('SELECT password, username, email FROM accounts WHERE id = ?');
+$stmt = $con->prepare('SELECT password, username, email, phone, address FROM accounts WHERE id = ?');
 // In this case we can use the account ID to get the account info.
 $stmt->bind_param('i', $_SESSION['id']);
 $stmt->execute();
-$stmt->bind_result($password, $username, $email);
+$stmt->bind_result($password, $username, $email, $phone, $address);
 $stmt->fetch();
 $stmt->close();
 if (!empty($_POST['old_password'])) {
@@ -29,14 +29,34 @@ if (!empty($_POST['old_password'])) {
 		exit('incorrect password!');
 	}
 	if (!empty($_POST['new_username'])) {
-		if ($con->query(sprintf('UPDATE `accounts` SET `username` = "%s" WHERE %d', $_POST['new_username'], $_SESSION['id'])) === TRUE) {
-			echo "username updated successfully";
-		}
+		$change_user = $con->prepare('UPDATE `accounts` SET `username` = (?) WHERE (?)');
+		$change_user->bind_param('ss', $_POST['new_username'], $_SESSION['id']);
+		$change_user->execute();
+		$change_user->close();
 	}
 	if (!empty($_POST['new_password'])) {
-		if ($con->query(sprintf('UPDATE `accounts` SET `password` = "%s" WHERE %d', password_hash($_POST['new_password'], PASSWORD_DEFAULT), $_SESSION['id'])) === TRUE) {
-			echo "password updated successfully";
-		}
+		$change_user = $con->prepare('UPDATE `accounts` SET `password` = (?) WHERE (?)');
+		$change_user->bind_param('ss', password_hash($_POST['new_password'], PASSWORD_DEFAULT), $_SESSION['id']);
+		$change_user->execute();
+		$change_user->close();
+	}
+	if (!empty($_POST['new_email'])) {
+		$change_user = $con->prepare('UPDATE `accounts` SET `email` = (?) WHERE (?)');
+		$change_user->bind_param('ss', $_POST['new_email'], $_SESSION['id']);
+		$change_user->execute();
+		$change_user->close();
+	}
+	if (!empty($_POST['new_phone'])) {
+		$change_user = $con->prepare('UPDATE `accounts` SET `phone` = (?) WHERE (?)');
+		$change_user->bind_param('ss', $_POST['new_phone'], $_SESSION['id']);
+		$change_user->execute();
+		$change_user->close();
+	}
+	if (!empty($_POST['new_address'])) {
+		$change_user = $con->prepare('UPDATE `accounts` SET `address` = (?) WHERE (?)');
+		$change_user->bind_param('ss', $_POST['new_address'], $_SESSION['id']);
+		$change_user->execute();
+		$change_user->close();
 	}
 	header('Location: profile.php');
 	exit;
@@ -71,24 +91,44 @@ if (!empty($_POST['old_password'])) {
 						<td>Email:</td>
 						<td><?=$email?></td>
 					</tr>
+					<tr>
+						<td>Phone:</td>
+						<td><?=$phone?></td>
+					</tr>
+					<tr>
+						<td>Address:</td>
+						<td><?=$address?></td>
+					</tr>
 				</table>
 				<form action = "profile.php" method = "post">
 					<table>
 						<tr>
 							<td>new username:</td>
 							<td> <input type="text" name = "new_username" placeholder="new username" id="username"> </td>
-							<td> <input type = "submit" value = "update profile info"> </td>
-						</tr>
-						<tr>
-							<td>old password: </td>
-							<td> <input type = "text" name = "old_password" placeholder = "old password" id = "password" required> </td> 
 						</tr>
 						<tr>
 							<td>new password: </td>
 							<td> <input type = "text" name = "new_password" placeholder = "new password" id = "password"> </td> 
 						</tr>
+						<tr>
+							<td>new email: </td>
+							<td> <input type = "text" name = "new_email" placeholder = "new email" id = "email"> </td> 
+						</tr>
+						<tr>
+							<td>new phone: </td>
+							<td> <input type = "text" name = "new_phone" placeholder = "new phone" id = "phone"> </td> 
+						</tr>
+						<tr>
+							<td>new address: </td>
+							<td> <input type = "text" name = "new_address" placeholder = "new address" id = "address"> </td> 
+						</tr>
+						<tr>
+							<td>old password (required): </td>
+							<td> <input type = "text" name = "old_password" placeholder = "old password" id = "password" required> </td> 
+							<td> <input type = "submit" value = "update profile info"> </td>
+						</tr>
 					</table>
-				</form>	
+				</form>
 			</div>
 		</div>
 	</body>
